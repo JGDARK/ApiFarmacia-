54ea0a698f186e774701847dc8a7257b2f9e836b
JG <JG@DESKTOP-UA3G2K4>
2019-08-14 20:22:09 -0400

    Model, Completado.

---
+++ ApiFarmacia/Model/Clientes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+    }
+}
---
+++ ApiFarmacia/Model/Proveedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Proveedor
+    {
+        public int Proveedorid { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
